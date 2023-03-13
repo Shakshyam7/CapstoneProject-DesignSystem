5accff675fa75c06739153ea7a321065fed5d5fb
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-03-13 06:32:32 -0700

    Adding colors value to the colors component and adjusting stypes

--- src/Components/Colors/Colors.js
+++ src/Components/Colors/Colors.js
@@ -7,21 +7,21 @@ function Colors() {
     <>
       <h6>Primary</h6>
       <div className="container">
-        <div className="rectangle rectangle1"></div>
-        <div className="rectangle rectangle2"></div>
-        <div className="rectangle rectangle3"></div>
+        <div className="rectangle rectangle1">HEX-000000(37%)</div>
+        <div className="rectangle rectangle2">HEX-000000(60%)</div>
+        <div className="rectangle rectangle3">HEX-030303</div>
       </div>
       <h6>secondary</h6>
       <div className="container">
-        <div className="rectangle rectangle4"></div>
-        <div className="rectangle rectangle5"></div>
-        <div className="rectangle rectangle6"></div>
+        <div className="rectangle rectangle4">HEX-FFFFFF(37%)</div>
+        <div className="rectangle rectangle5">HEX-FFFFFF(60%)</div>
+        <div className="rectangle rectangle6">HEX-FFFFFF(87%)</div>
       </div>
       <h6>Tertiary</h6>
       <div className="container">
-        <div className="rectangle rectangle7"></div>
-        <div className="rectangle rectangle8"></div>
-        <div className="rectangle rectangle9"></div>
+        <div className="rectangle rectangle7">HEX-D1516F(60%)</div>
+        <div className="rectangle rectangle8">HEX-D1516F(87%)</div>
+        <div className="rectangle rectangle9">HEX-D1516F</div>
       </div>
     </>
   );
