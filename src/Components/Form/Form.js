ce16b925aa78811a3b2ace2c2a8ea03ff42f11bf
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-03-14 04:07:35 -0700

    Adjusting styles change the textfield name to className

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -7,43 +7,44 @@ function Form() {
   return (
     <>
       <div className="form__container">
-        <div className="form__header">
+        <div classNameName="form__header">
           <h6>Fill out the form below and leave us a messege!</h6>
-
-          <div className="form__text1">
-            <Textfield
-              name={"label__textfield"}
-              class={"textfield"}
-              placeholder={"Firstname"}
-            />
-            <Textfield
-              name={"label__textfield"}
-              class={"textfield"}
-              placeholder={"Lastname"}
-            />
-          </div>
-          <div className="form__text2">
-            <Textfield
-              name={"label__textfield"}
-              class={"textfield"}
-              placeholder={"Email"}
-            />
-          </div>
-          <div className="form__text2">
-            <Textfield
-              name={"label__textfield"}
-              class={"textfield"}
-              placeholder={"Number"}
-            />
-          </div>
-          <div className="form__text3">
-            <textarea
-              className="label__textarea"
-              placeholder="Message"
-            ></textarea>
+          <div className="formTextInputContainer">
+            <div className="form__text1">
+              <Textfield
+                name={"label__textfield"}
+                className={"textfield"}
+                placeholder={"Firstname"}
+              />
+              <Textfield
+                name={"label__textfield"}
+                className={"textfield"}
+                placeholder={"Lastname"}
+              />
+            </div>
+            <div className="form__text2">
+              <Textfield
+                name={"label__textfield"}
+                className={"textfield"}
+                placeholder={"Email"}
+              />
+            </div>
+            <div className="form__text2">
+              <Textfield
+                name={"label__textfield"}
+                className={"textfield"}
+                placeholder={"Number"}
+              />
+            </div>
+            <div className="form__text3">
+              <textarea
+                className="label__textarea"
+                placeholder="Message"
+              ></textarea>
+            </div>
           </div>
           <div className="button__send">
-            <Button class={"button__primary"} name={"SEND"} />
+            <Button className={"button__primary"} name={"SEND"} />
           </div>
         </div>
       </div>
