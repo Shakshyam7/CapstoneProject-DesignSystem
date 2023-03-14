c0df9c80435d480785b4c58501baa348ca192d7a
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-03-14 04:49:28 -0700

    renaming the classname

--- src/Components/Colors/Colors.js
+++ src/Components/Colors/Colors.js
@@ -8,7 +8,7 @@ function Colors() {
       <div>
         <div className="container">
           <h6>Primary</h6>
-          <div className="primaryColorContainer">
+          <div className="rectangleColorContainer">
             <div className="rectangle rectangle1">HEX-000000(37%)</div>
             <div className="rectangle rectangle2">HEX-000000(60%)</div>
             <div className="rectangle rectangle3">HEX-030303</div>
@@ -16,7 +16,7 @@ function Colors() {
         </div>
         <div className="container">
           <h6>secondary</h6>
-          <div className="primaryColorContainer">
+          <div className="rectangleColorContainer">
             <div className="rectangle rectangle4">HEX-FFFFFF(37%)</div>
             <div className="rectangle rectangle5">HEX-FFFFFF(60%)</div>
             <div className="rectangle rectangle6">HEX-FFFFFF(87%)</div>
@@ -24,7 +24,7 @@ function Colors() {
         </div>
         <div className="container">
           <h6>Tertiary</h6>
-          <div className="primaryColorContainer">
+          <div className="rectangleColorContainer">
             <div className="rectangle rectangle7">HEX-D1516F(60%)</div>
             <div className="rectangle rectangle8">HEX-D1516F(87%)</div>
             <div className="rectangle rectangle9">HEX-D1516F</div>
