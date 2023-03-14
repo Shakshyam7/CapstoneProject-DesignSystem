6f6bd0dc0083a8356985606288b75b081f2315b3
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-03-14 04:44:45 -0700

    Adjusting the styles of the color component added the primary container in the div to make it responsive for mobile

--- src/Components/Colors/Colors.js
+++ src/Components/Colors/Colors.js
@@ -5,23 +5,27 @@ import "./Colors.css";
 function Colors() {
   return (
     <>
-      <h6>Primary</h6>
-      <div className="container">
-        <div className="rectangle rectangle1">HEX-000000(37%)</div>
-        <div className="rectangle rectangle2">HEX-000000(60%)</div>
-        <div className="rectangle rectangle3">HEX-030303</div>
-      </div>
-      <h6>secondary</h6>
-      <div className="container">
-        <div className="rectangle rectangle4">HEX-FFFFFF(37%)</div>
-        <div className="rectangle rectangle5">HEX-FFFFFF(60%)</div>
-        <div className="rectangle rectangle6">HEX-FFFFFF(87%)</div>
-      </div>
-      <h6>Tertiary</h6>
-      <div className="container">
-        <div className="rectangle rectangle7">HEX-D1516F(60%)</div>
-        <div className="rectangle rectangle8">HEX-D1516F(87%)</div>
-        <div className="rectangle rectangle9">HEX-D1516F</div>
+      <div>
+        <div className="container">
+          <h6>Primary</h6>
+          <div className="primaryColorContainer">
+            <div className="rectangle rectangle1">HEX-000000(37%)</div>
+            <div className="rectangle rectangle2">HEX-000000(60%)</div>
+            <div className="rectangle rectangle3">HEX-030303</div>
+          </div>
+        </div>
+        <div className="container">
+          <h6>secondary</h6>
+          <div className="rectangle rectangle4">HEX-FFFFFF(37%)</div>
+          <div className="rectangle rectangle5">HEX-FFFFFF(60%)</div>
+          <div className="rectangle rectangle6">HEX-FFFFFF(87%)</div>
+        </div>
+        <div className="container">
+          <h6>Tertiary</h6>
+          <div className="rectangle rectangle7">HEX-D1516F(60%)</div>
+          <div className="rectangle rectangle8">HEX-D1516F(87%)</div>
+          <div className="rectangle rectangle9">HEX-D1516F</div>
+        </div>
       </div>
     </>
   );
