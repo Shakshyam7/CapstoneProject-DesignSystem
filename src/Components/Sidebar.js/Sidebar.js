ba1ff8bd5fc0b78f043bf3489b432d176cf2b698
Shakshyam7 <90713152+Shakshyam7@users.noreply.github.com>
2023-03-13 23:18:20 -0700

    Wrapping the list item with link to take the user to the clicked  component

--- src/Components/Sidebar.js/Sidebar.js
+++ src/Components/Sidebar.js/Sidebar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Sidebar.css";
 
 function Sidebar({ sidebarItems }) {
@@ -7,9 +8,8 @@ function Sidebar({ sidebarItems }) {
       <div className="sidebarContainer">
         <ul className="sidebarUl">
           {sidebarItems.map((item, index) => (
-            
             <li className="sidebarItem" key={index}>
-              {item}
+              <Link to={item} className = "sidebarLink">{item}</Link> 
             </li>
           ))}
         </ul>
